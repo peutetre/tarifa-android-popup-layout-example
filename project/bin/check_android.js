27cbb1acdd816d45ed1237aa429508878283f404
Paul Panserrieu <paul.panserrieu@gmail.com>
2015-09-14 16:01:56 +0200

    custom popup theme

--- project/bin/check_android.js
+++ project/bin/check_android.js
@@ -5,6 +5,8 @@ var Q = require('q'),
 
 module.exports = function (msg) {
     var manifest = path.join(__dirname, '../../app/platforms/android/AndroidManifest.xml'),
+        src = path.join(__dirname, 'popup.xml'),
+        theme = path.join(__dirname, '../../app/platforms/android/res/values/popup.xml'),
         defer = Q.defer(),
         xml = fs.readFileSync(manifest, 'utf-8');
 
@@ -13,14 +15,16 @@ module.exports = function (msg) {
             defer.reject(err);
         }
         else {
-            // result.manifest.application[0].$['android:theme'] = "@android:style/Theme.Dialog";
-            result.manifest.application[0].$['android:theme'] = "@android:style/Theme.Holo.Dialog.NoActionBar";
+            // "@android:style/Theme.Dialog"
+            // "@android:style/Theme.Holo.Dialog.NoActionBar"
+            result.manifest.application[0].$['android:theme'] = "@style/PopupTheme";
             delete result.manifest.application[0].activity[0].$['android:theme'];
             var builder = new xml2js.Builder({
                 renderOpts: { pretty: true, indent: '    ', newline: '\n' },
                 headless: true
             });
             fs.writeFileSync(manifest, builder.buildObject(result), 'utf-8');
+            fs.writeFileSync(theme, fs.readFileSync(src, 'utf-8'), 'utf-8');
             defer.resolve(msg);
         }
     });
